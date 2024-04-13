a4593621e41d25a848623225449613b6b74ae82f
Alikhan <alikhanabishev@gamil.com>
2024-04-13 16:35:51 +0600

    added hashing

--- backend/Lorby/register/models.py
+++ backend/Lorby/register/models.py
@@ -5,3 +5,5 @@ class User(models.Model):
     login = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
     email = models.EmailField()
+
+
--- backend/Lorby/register/views.py
+++ backend/Lorby/register/views.py
@@ -4,16 +4,21 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import User
 from .serializers import UserSerializer
+from django.contrib.auth.hashers import PBKDF2PasswordHasher
 
 
+def hasher(PBKDF2PasswordHasher):
+    iterations = PBKDF2PasswordHasher.iterations * 100
+    return iterations
 @api_view(['GET', 'POST'])
 def register(request):
 
         if request.method == 'POST':
             user = request.data
+            user['password'] = hasher(user['password'])
             user_sql = User(login=user['login'], password = user['password'], email = user['email'])
             user_sql.save()
 
             return Response({"data" : user})
         
-        return Response({"data"  : "error"})
+        return Response({"data" : "error"})
