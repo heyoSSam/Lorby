8fb4f68febdc6d575d2cbe95252d9fc1971f1543
heyossam <heyossam11@gmail.com>
2024-04-13 13:20:45 +0500

    added basic logic of registering

--- backend/Lorby/Lorby/settings.py
+++ backend/Lorby/Lorby/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME' : 'Lorby',
         'USER' : 'postgres',
-        'PASSWORD' : 'rootroot',
+        'PASSWORD' : 'heyoSSam777',
         'HOST' : 'localhost',
         'POST' : '5432'
     }
--- backend/Lorby/register/views.py
+++ backend/Lorby/register/views.py
@@ -6,14 +6,14 @@ from .models import User
 from .serializers import UserSerializer
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def register(request):
-    if request.method == 'GET':
-        posts = User.objects.all()
-        serializer = UserSerializer(posts, many=True)
-        return Response({"data": serializer.data})
 
-    elif request.method == 'POST':
-        user = request.data
-        user_sql = User(login=user.login, password = user.password, email = user.email)
-        user_sql.save()
+        if request.method == 'POST':
+            user = request.data
+            user_sql = User(login=user['login'], password = user['password'], email = user['email'])
+            user_sql.save()
+
+            return Response({"data" : user})
+        
+        return Response({"data"  : "error"})
